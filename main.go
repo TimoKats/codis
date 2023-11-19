264acc7617052a2fe3479aa1fb47bf4d266c1c7c
TimoKats <tpakats@gmail.com>
2023-11-19 16:12:26 +0100

    better layout

--- main.go
+++ main.go
@@ -27,10 +27,18 @@ type Styles struct {
 	InputField lipgloss.Style
 }
 
-func DefaultStyles(color int) *Styles {
+func QueryStyle(color int) *Styles {
 	s := new(Styles)
 	s.BorderColor = lipgloss.Color(strconv.Itoa(color)) // 10, 11, 12
 	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80)
+	return s
+}
+
+
+func ResultStyle() *Styles {
+	s := new(Styles)
+	s.BorderColor = lipgloss.Color("0") 
+	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80).Height(10)
 	return s
 }
 
@@ -40,9 +48,10 @@ type model struct {
 	query Query
 	width int
 	height int
-	answerField textinput.Model
+	queryField textinput.Model
 	resultField textarea.Model
-	styles *Styles
+	queryStyle *Styles
+	resultStyle *Styles
 }
 
 type Query struct {
@@ -52,11 +61,15 @@ type Query struct {
 }
 
 func New(query Query) *model {
-	styles := DefaultStyles(11)
-	answerField := textinput.New() 
-	answerField.Placeholder = "press / to start querying..."
+	queryStyle := QueryStyle(11)
+	resultStyle := ResultStyle()
+	queryField := textinput.New() 
+	queryField.Placeholder = "press / to start querying..."
 	resultField := textarea.New()
-	return &model{queryIndex: 0, resultIndex:0, query: query, answerField: answerField, resultField: resultField, styles: styles}
+	return &model{queryIndex: 0, resultIndex:0, query: query, 
+		queryField: queryField, resultField: resultField, 
+		queryStyle: queryStyle, resultStyle: resultStyle,
+	}
 }
 
 func (m model) Init() tea.Cmd {
@@ -74,44 +87,51 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				case "ctrl+c":
 					return m, tea.Quit
 				case "/":
-					m.answerField.Focus()
+					m.queryField.Focus()
 					return m, nil
 				case "esc":
-					m.answerField.Reset()
+					m.queryField.Reset()
 					m.resultField.Reset()
 					return m, nil
 				case "enter":
-					m.query.query = m.answerField.Value()
-					m.answerField.Reset()
+					m.query.query = m.queryField.Value()
+					m.queryField.Reset()
 					m.query.result = cosearch.BasicQuery(m.query.query, labeledRows, orderedKeys)
 					m.resultField.SetValue(m.query.result[m.resultIndex])
 					return m, nil
-				case ">":
-					m.answerField.Reset()
+				case "ctrl+j":
+					m.queryField.Reset()
 					m.queryIndex = (m.queryIndex + 1) % 2
-					m.styles = DefaultStyles(((m.queryIndex + 1) % 2) + 10)
-				case "n":
-					m.answerField.Reset()
+					m.queryStyle = QueryStyle(((m.queryIndex + 1) % 2) + 10)
+				case "ctrl+k":
+					m.queryField.Reset()
 					m.resultField.Reset()
 					m.resultIndex = (m.resultIndex + 1) % len(m.query.result)
 					m.resultField.SetValue(m.query.result[m.resultIndex])
 			}
 	}
-	m.answerField, cmd = m.answerField.Update(msg)
+	m.queryField, cmd = m.queryField.Update(msg)
 	return m, cmd	
 }
 
 func (m model) View() string {
-	return lipgloss.Place(
+  return lipgloss.Place(
 		m.width,
 		m.height,
 		lipgloss.Center,
 		lipgloss.Top,
 		lipgloss.JoinVertical(
 			lipgloss.Center, 
-			m.query.queryType[m.queryIndex], 
-			m.styles.InputField.Render(m.answerField.View()),
-			m.resultField.View(),
+			lipgloss.JoinVertical(
+				lipgloss.Left,
+				m.query.queryType[m.queryIndex],
+				m.queryStyle.InputField.Render(m.queryField.View()),
+			),
+			lipgloss.JoinVertical(
+				lipgloss.Right,
+				m.resultStyle.InputField.Render(m.resultField.View()),
+				"codis v0.0.0 by Timo Kats 2023 (press ctrl+c to quit)",
+			),
 		),
 	)
 }
@@ -139,7 +159,7 @@ func tempCodis() {
 
 func main() {
 	tempCodis()
-	query := Query{"", []string{}, []string{"basic search", "explorative seach"}}
+	query := Query{"", []string{"None"}, []string{"basic search", "explorative seach"}}
 	m := New(query)
 	p := tea.NewProgram(m, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
